c7487271a3cf5a9b8a2c676afc8f241586634971
PratikshaJadhav23 <jadhav23pratiksha@gmail.com>
2026-03-16 16:43:56 +0530

    Added table_schema of employees_data

---
+++ database_schema/create_tables.sql
@@ -0,0 +1,18 @@
+use practice1;
+CREATE TABLE employees_data (
+employee_id INT,
+name VARCHAR(50),
+age INT,
+department VARCHAR(50),
+salary INT,
+city VARCHAR(50),
+experience_years INT,
+project_count INT,
+performance_score FLOAT,
+product_purchased VARCHAR(50),
+purchase_amount INT,
+order_status VARCHAR(20),
+rating INT,
+bonus INT,
+tax_percent FLOAT
+);
